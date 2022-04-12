bc7401e5188d4fe493469dc7fb8836236a4e8b56
Quan Thien Bao Tran <tranthienbaoquan@gmail.com>
2022-04-12 12:23:40 +0200

    update draft

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -7,6 +7,8 @@ use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_core::{sr25519, Pair, Public};
 use sp_finality_grandpa::AuthorityId as GrandpaId;
 use sp_runtime::traits::{IdentifyAccount, Verify};
+use hex_literal::hex;
+use sp_core::crypto::UncheckedInto;
 
 // The URL for the telemetry server.
 // const STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
@@ -126,6 +128,96 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 
 /// Configure initial storage state for FRAME modules.
 fn testnet_genesis(
+	wasm_binary: &[u8],
+	initial_authorities: Vec<(AuraId, GrandpaId)>,
+	root_key: AccountId,
+	endowed_accounts: Vec<AccountId>,
+	_enable_println: bool,
+) -> GenesisConfig {
+	GenesisConfig {
+		system: SystemConfig {
+			// Add Wasm runtime to storage.
+			code: wasm_binary.to_vec(),
+		},
+		balances: BalancesConfig {
+			// Configure endowed accounts with initial balance of 1 << 60.
+			balances: endowed_accounts.iter().cloned().map(|k| (k, 1 << 60)).collect(),
+		},
+		aura: AuraConfig {
+			authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
+		},
+		grandpa: GrandpaConfig {
+			authorities: initial_authorities.iter().map(|x| (x.1.clone(), 1)).collect(),
+		},
+		sudo: SudoConfig {
+			// Assign network admin rights.
+			key: Some(root_key),
+		},
+		transaction_payment: Default::default(),
+	}
+}
+
+pub fn kitty_testnet_config() -> Result<ChainSpec, String> {
+	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
+
+	let initial_authorities: Vec<(AuraId, GrandpaId)> = vec![
+		(
+			hex!("6a1953567adad4b090b6ced2d669a44a7f7b57fbdd1562d8e23079daa7745d5e").unchecked_into(),
+			hex!("d5cbcaa4d13f611e6af7a10587f89a8cd57b093761f8da4fc55d16e103a5d2ef").unchecked_into()
+		),
+		(
+			hex!("e46c2a6aefac2ccd546b16e0bc3d48b9928871594140691b3937e324f7037f74").unchecked_into(),
+			hex!("89bda569ac96241376d79653e1f01fff1f2c685146d07a4b208f8e8245ff331e").unchecked_into()
+		),
+		(
+			hex!("7a08c7a5fea98d9394e9870db7adedd8a2a8e948a62737c1022dae5514e97029").unchecked_into(),
+			hex!("5f195138996cd09f99227ad3b8921f2b2b3ea7b527f49949b72a6468e1c74eb3").unchecked_into()
+		),
+	];
+	Ok(ChainSpec::from_genesis(
+		// Name
+		"Kitty Testnet",
+		// ID
+		"kitty",
+		ChainType::Live,
+		move || {
+			kitty_testnet_genesis(
+				wasm_binary,
+				// Initial PoA authorities
+				initial_authorities.clone(),
+				// Sudo account
+				hex!("90faed2f82ec46fb444279a8993f0944e5a1b12b32b1cfb29a3dc1fcc4e44d37").into(),
+				// Pre-funded accounts
+				vec![
+					hex!("90faed2f82ec46fb444279a8993f0944e5a1b12b32b1cfb29a3dc1fcc4e44d37").into(),
+					hex!("6a1953567adad4b090b6ced2d669a44a7f7b57fbdd1562d8e23079daa7745d5e").into(),
+					hex!("e46c2a6aefac2ccd546b16e0bc3d48b9928871594140691b3937e324f7037f74").into(),
+					hex!("7a08c7a5fea98d9394e9870db7adedd8a2a8e948a62737c1022dae5514e97029").into(),
+				],
+				true,
+			)
+		},
+		// Bootnodes
+		vec![],
+		// Telemetry
+		None,
+		// Protocol ID
+		None,
+		None,
+		// Properties
+		Some(
+			serde_json::from_str(
+				"{\"tokenDecimals\": 18, \"tokenSymbol\": \"KITTY\", \"SS58Prefix\": 48}",
+			)
+			.expect("Provided valid json map"),
+		),
+		// Extensions
+		None,
+	))
+}
+
+/// Configure initial storage state for FRAME modules.
+fn kitty_testnet_genesis(
 	wasm_binary: &[u8],
 	initial_authorities: Vec<(AuraId, GrandpaId)>,
 	root_key: AccountId,
---
+++ scripts/keys/run.sh
@@ -0,0 +1,8 @@
+curl http://localhost:9933 -H "Content-Type:application/json;charset=utf-8" -d @aura1
+curl http://localhost:9933 -H "Content-Type:application/json;charset=utf-8" -d @gran1
+
+curl http://localhost:9934 -H "Content-Type:application/json;charset=utf-8" -d @aura2
+curl http://localhost:9934 -H "Content-Type:application/json;charset=utf-8" -d @gran2
+
+curl http://localhost:9935 -H "Content-Type:application/json;charset=utf-8" -d @aura3
+curl http://localhost:9935 -H "Content-Type:application/json;charset=utf-8" -d @gran3
